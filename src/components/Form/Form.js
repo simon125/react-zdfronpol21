c24ad185d9658cee5d3b6c225942ab39dbd1488e
Szymon Oleszek <szymqwerty@gmail.com>
2023-04-02 07:57:51 +0200

    struktura folderow i plikow

---
+++ src/components/Form/Form.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Form = () => {
+  return (
+    <astricle>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <button>Submit</button>
+      </form>
+    </astricle>
+  );
+};
